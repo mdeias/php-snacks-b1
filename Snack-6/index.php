6f137b06ff3112bff928367be6ccb1244595a597
mdeias <mdeias@icloud.com>
2021-12-21 18:22:18 +0100

    snack 6

--- Snack-6/index.php
+++ Snack-6/index.php
@@ -41,13 +41,56 @@ $db = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        h2{
+            margin-bottom: 40px;
+        }
+        ul{
+            list-style: none;
+        }
+        .insegnanti, .pm {
+            float: left;
+            padding: 40px;
+            text-align: center;
+            width: 50%;    
+            color: white;
+            height: 300px;
+        }
+        .pm{
+            background-color: green;
+        }
+        .insegnanti{
+            background-color: gray;
+        }
+    </style>
 </head>
 <body>
     <div class="insegnanti">
-
+      <h2>Teachers</h2>
+        <ul>
+            <?php foreach ($db['teachers'] as $teacher){ ?>
+                <li>
+                    <?php echo $teacher['name'];?>
+                    <?php echo $teacher['lastname'];?>
+                </li>
+            <?php }; ?>
+        </ul>
     </div>
-    <div class="studenti">
-
+    <div class="pm">
+        <h2>Pm</h2>
+        <ul>
+            <?php foreach ($db['pm'] as $sPm){ ?>
+                <li>
+                    <?php echo $sPm['name'];?>
+                    <?php echo $sPm['lastname'];?>
+                </li>
+            <?php }; ?>
+        </ul>
     </div>
 </body>
 </html>
